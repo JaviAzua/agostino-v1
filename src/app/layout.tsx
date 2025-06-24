8479f03504c99afb0738d1e52b4e03af6c219eed
Javier Azua <azua.javi@gmail.com>
2025-06-24 12:41:47 -0300

    Remove unused profile image, clean up globals.css by deleting unnecessary styles, and add a new HomePage component for the main application layout.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import "./globals.css";
 import { Roboto } from "next/font/google";
 import { Metadata } from "next";
-import { Toaster } from "@/components/ui/toaster";
 import { env } from "../../env";
 
 export const metadata: Metadata = {
@@ -101,11 +100,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" className="h-full">
-      <body className={`${roboto.className} bg-blackB text-white`}>
-        {children}
-        <Toaster />
-      </body>
+    <html lang="en">
+      <body className={`${roboto.className}`}>{children}</body>
     </html>
   );
 }
